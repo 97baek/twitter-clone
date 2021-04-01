49c6e56459f5f41c4b424fb088bba20a8411d0e7
BAEK SANGHEUN <170410_@naver.com>
2021-04-01 16:57:53 +0900

    # firestore에 tweet 등록

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { dbService } from "fbase";
 
 const Home = () => {
-  return <div>Home</div>;
+  console.log(dbService);
+  const [tweet, setTweet] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dbService.collection("tweets").add({
+      tweet,
+      createdAt: new Date(),
+    });
+    setTweet("");
+  };
+
+  const onChange = (e) => {
+    setTweet(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="tweet"
+          value={tweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
